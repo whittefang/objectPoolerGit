be7d129fc869592ac973f758c84daa2432428fba
whittefang <whittefang@gmail.com>
2015-11-11 19:41:46 -0600

    initialcommit

---
+++ object pooler example/Assets/ObjectSelfDesruct.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSelfDesruct : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	void OnEnable(){
+		Invoke ("TurnOff", 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void TurnOff(){
+		gameObject.SetActive( false);
+	}
+}
---
+++ object pooler example/Assets/Shoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	public ObjectPoolScript BulletPool;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// check for input to shoot bullet
+		if (Input.GetButtonDown("Fire1")){
+
+			// get object from the pool
+			GameObject NewBullet = BulletPool.FetchObject();
+
+			// put new object ontop of object that is shooting it
+			NewBullet.transform.position = transform.position;
+
+			// turn object on
+			NewBullet.SetActive(true);
+
+			// add velocity so that bullet moves
+			NewBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+
+
+		}
+	}
+}
